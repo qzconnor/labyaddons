3671616bfe17dadaef405a19e40a46792a4b37c9
xConnorTV <46536197+qzconnor@users.noreply.github.com>
2021-07-10 22:42:07 +0200

    init

---
+++ index.js
@@ -0,0 +1,99 @@
+require('dotenv').config();
+
+const SERVER_PORT = process.env.SERVER_PORT | 3000;
+
+const express = require('express');
+const exphbs = require("express-handlebars");
+const app = express();
+const bodyParser = require("body-parser");
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "addons"
+});
+
+var connection = false;
+
+con.connect(function(err) {
+    if (err) throw err;
+    connection = true;
+});
+
+
+
+app.use(bodyParser.json());
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.use('/static', express.static('public'));
+app.use(bodyParser.urlencoded({extended: true})); 
+
+
+app.get("/", async (req, res) => {
+    res.render('index',{
+        title: req.hostname + " - Showup"
+    });
+})
+
+app.post("/api/upload", async (req, res) => {
+})
+
+
+
+app.get("/api/inoffical", async (req, res) => {
+    if(connection){
+        con.query("SELECT * FROM inoffical", function (err, result) {
+            if (err) throw err;
+            var addons18 = [];
+            var addons112 = [];
+            var addons116 = [];
+            for(var addon of result){
+                if(addon.version == 18){
+                    addons18.push({
+                        "name": addon.name,
+                        "uuid": addon.uuid,
+                        "uploadedAt": addon.uploadedAt,
+                        "status": addon.status,
+                        "author": addon.author,
+                        "description": addon.description,
+                        "dl": addon.dl
+                    })
+                }else if(addon.version == 112){
+                    addons112.push({
+                        "name": addon.name,
+                        "uuid": addon.uuid,
+                        "uploadedAt": addon.uploadedAt,
+                        "status": addon.status,
+                        "author": addon.author,
+                        "description": addon.description,
+                        "dl": addon.dl
+                    })
+                }else if(addon.version == 116){
+                    addons116.push({
+                        "name": addon.name,
+                        "uuid": addon.uuid,
+                        "uploadedAt": addon.uploadedAt,
+                        "status": addon.status,
+                        "author": addon.author,
+                        "description": addon.description,
+                        "dl": addon.dl
+                    })
+                }
+             
+            }
+            res.json({
+                "addons": {
+                    "18": addons18,
+                    "112": addons112,
+                    "116": addons116
+                }
+            })
+        });
+    }
+})
+app.listen(SERVER_PORT, ()=>{
+    console.log(`Server listening on port: ${SERVER_PORT}.`);
+    console.log(`SITE: http://localhost:${SERVER_PORT}`);
+}) 
---
+++ public/js/app.js
@@ -0,0 +1,225 @@
+//ICON https://dl.labymod.net/latest/addons/${UUID}/icon.png 
+
+var addonTemplate = document.getElementById("addon-template");
+
+var searchObj = {
+    "offical": {},
+    "inoffical": {},
+    "show-offical": {},
+    "show-inoffical": {}
+};
+var tab = "offical";
+var beforTab = tab;
+var v = "18";
+$(window).ready( async() => { 
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const params = Object.fromEntries(urlSearchParams.entries());
+
+    if(params.tab){
+        tab = params.tab;
+    }
+    if(params.v){
+        v = params.v;
+        $(`#${tab}-version`).val(v);
+    }
+    await fetch(v); 
+    document.getElementById(tab).click();
+    if(params.search && params.search !== ""){
+        var search = params.search;
+        var realTab = document.querySelectorAll(`[data-to='${tab}']`)[0];
+        var bar =  $(realTab).find('.search').get(0);
+        bar.value = search;
+        searchAddons(search);
+    }
+
+    $('#offical-search').on('input', (e) => {
+        searchAddons(e.target.value)
+        changeURL()
+    })
+    
+    $('#inoffical-version').on('change', async (e)=>{
+        await fetch(e.target.value);  
+        searchAddons($(`#${tab}-search`).val());
+        changeURL()
+    })
+    $('#offical-version').on('change', async (e)=>{
+        await fetch(e.target.value);  
+        searchAddons($(`#${tab}-search`).val());
+        changeURL()
+    })
+    $('#inoffical-search').on('input', (e) => {
+        searchAddons(e.target.value)
+        changeURL()
+    })
+    
+    function searchAddons(q){
+        if(q === ""){
+            searchObj["show-" + tab] = searchObj[tab];
+            drawAddons(tab + "-addons",searchObj["show-" + tab]);
+        }else{
+            searchObj["show-" + tab] = getAddonsStartsWith(searchObj[tab],q);
+            drawAddons(tab + "-addons",searchObj["show-" + tab]);
+        }
+    }
+
+    $(".tablinks").click(async () => {
+        await fetch(v);  
+        changeURL()
+    })
+    
+    function drawAddons(where,object){
+        var w =  document.getElementById(where);
+        removeAllChildNodes(w)
+        if(Object.keys(object).length === 0 && object.constructor === Object){
+            var ele = document.createElement('p');
+            ele.setAttribute("id", "notFound");
+            ele.innerHTML = "No matching addon found!"
+            w.appendChild(ele)
+        }else{
+            for(var t in object){
+                var addon = object[t];
+                var clon = addonTemplate.content.cloneNode(true);
+                clon.getElementById('icon').src = addon.url
+                clon.getElementById('icon').alt = addon.t
+                clon.getElementById('name').innerHTML = t;
+                clon.getElementById('author').innerHTML = addon.author
+                clon.getElementById('description').innerHTML = addon.description
+                clon.getElementById('download').setAttribute('data-uuid', addon.uuid);
+                if(addon.offical){
+                    clon.getElementById('download').setAttribute('data-url', `https://dl.labymod.net/latest/?file=${addon.uuid}&a=1`);
+                }else{
+                    clon.getElementById('download').setAttribute('data-url', addon.dl);
+                }
+               
+                w.appendChild(clon);
+            }
+        }
+        
+    }
+    
+    function getAddonsStartsWith(object, str) {
+        var addonResult = {};
+        for (var property in object) {
+            if (object.hasOwnProperty(property) && 
+               property.toString().startsWith(str)) {
+               addonResult[property] = object[property];
+            }
+        }
+        return addonResult
+    }
+    
+    async function fetch(version) {
+        searchObj["offical"] = {};
+        searchObj["inoffical"] = {};
+        searchObj["show-offical"] = {};
+        searchObj["show-inoffical"] = {};
+        var data = await $.getJSON( "./static/addons.json");
+
+
+        for(var entry of data.addons[version]){
+            searchObj["offical"][entry.name] = {
+                "url": `https://dl.labymod.net/latest/addons/${entry.uuid}/icon.png`,
+                "author": "by " + entry.author,
+                "description": entry.description,
+                "uuid": entry.uuid,
+                "offical": true,
+                "dl": entry.dl
+            }
+        }
+        searchObj["show-offical"] = searchObj["offical"];
+        drawAddons("offical-addons", searchObj["show-offical"]);
+        data = await $.getJSON( "./api/inoffical");    
+        
+        for(var entry of data.addons[version]){
+            searchObj["inoffical"][entry.name] = {
+                "url": `static/logos/${entry.uuid}.png`,
+                "author": "by " + entry.author,
+                "description": entry.description,
+                "uuid": entry.uuid,
+                "offical": false,
+                "dl": entry.dl
+            }
+        } 
+        searchObj["show-inoffical"] = searchObj["inoffical"];
+        drawAddons("inoffical-addons",searchObj["show-inoffical"]);
+    }
+
+})
+
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+function openTab(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+    beforTab = tab;
+    tab = cityName.replace("store_", "");
+
+}    
+
+function changeURL(){
+    var searchVal = $(`#${tab}-search`).val();
+    var version = $(`#${tab}-version`).val();
+    changeQueryString(`?search=${searchVal}&tab=${tab}&v=${version}`,undefined);
+}
+function changeQueryString(searchString, documentTitle){      
+    documentTitle = typeof documentTitle !== 'undefined' ? documentTitle : document.title;      
+    var urlSplit=( window.location.href ).split( "?" );      
+    var obj = { Title: documentTitle, Url: urlSplit[0] + searchString };      
+    history.pushState(obj, obj.Title, obj.Url);      
+}
+
+function download(el){
+    window.open(el.getAttribute('data-url'), "_blank");
+}
+function share(){
+    copyTextToClipboard(window.location.href)
+}
+
+
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+  
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+  
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'successful' : 'unsuccessful';
+      console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+      console.error('Fallback: Oops, unable to copy', err);
+    }
+  
+    document.body.removeChild(textArea);
+  }
+  function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+      fallbackCopyTextToClipboard(text);
+      return;
+    }
+    navigator.clipboard.writeText(text).then(function() {
+      console.log('Async: Copying to clipboard was successful!');
+    }, function(err) {
+      console.error('Async: Could not copy text: ', err);
+    });
+  }
