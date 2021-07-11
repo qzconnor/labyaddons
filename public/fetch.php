6800153d533b34485fbf77e29001fb126bc34b15
l3nnartt <loesche.lennart@gmail.com>
2021-07-11 18:26:25 +0200

    Fetch PHP for Addon.json

---
+++ public/fetch.php
@@ -0,0 +1,7 @@
+<?php
+
+$date = date_create();
+$data = file_get_contents("https://dl.labymod.net/addons.json");
+$content = json_decode($data, true);
+$content["time"] = date_timestamp_get($date);
+file_put_contents("addons.json", json_encode($content));
